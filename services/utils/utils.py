72152bc544b45161740357a5e09cff178f54fa0a
wty81213 <wty81213@hotmail.com>
2023-08-23 23:04:10 +0800

    combine all message in 1 minute before model prediction

--- services/utils/utils.py
+++ services/utils/utils.py
@@ -1,13 +1,17 @@
 import json
 import time
+import os
 
 NUM_CLASSES = 20
 
-with open("config/config.json", "r") as f:
-        classes = json.load(f)["classes"]
+IS_HUMAN_REPLY_ACTIVE_LIST = ["請稍候，我們將盡速為您服務，不好意思造成您的不便", "真人客服", "真人回覆", "請稍候，將由真人客服為您排除機台異常"]
+                                                                                               
+# /Users/wty81213/Documents/project/ML/iTrash/services/utils/utils.py
+with open(os.getcwd() + "/config/config.json", "r") as f:
+        content = json.load(f)
 
 def question_options():
-      return classes
+      return content["classes"]
 
 
 def find_category_by_id(subclass, target_id):
@@ -26,7 +30,7 @@ def answer_response(id):
    
     found_category = {}
 
-    for subclass in classes:
+    for subclass in content["classes"]:
        
         found_category = find_category_by_id(subclass, id)
 
@@ -45,12 +49,39 @@ def is_user_inactive(last_input_time, current_time, inactive_threshold):
     
     return time_difference > inactive_threshold
 
-def is_skip_predict(last_input_time, current_time, time_interval=5):
+
+
+
+def read_json_file(key):
+    return content[key]
+        # no_id_list = content["key"]
+        # human_customer_service = content["human_customer_service"]
+
+
+def is_change_to_human_customer_service(text):
+    return text in IS_HUMAN_REPLY_ACTIVE_LIST
+    #  return text == "請稍候，我們將盡速為您服務，不好意思造成您的不便" or "真人客服" in text
+
+
+def is_skip_predict(last_input_time, current_time, time_interval=30):
     # current_time = time.time()
     
     time_difference = current_time - last_input_time
+
+    # print("current_time: ", current_time)
+
+    # print("last_input_time: ", last_input_time)
+
+    # print("time difference: ", time_difference)
+
+    # print("====================================")
     
     return time_difference < time_interval
+
+
+def clear_queue(queue):
+    while not queue.empty():
+        queue.get()
 
 
 
